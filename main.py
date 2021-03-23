e89fba62e2be5b2290bb5c7d9c93613e9ad4a101
hoseacodes <dominique11h@yahoo.com>
2021-03-22 23:42:43 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Sonkkick
+
+# Artist Search
+# https://api.songkick.com/api/3.0/search/artists.json?apikey={your_api_key}&query={artist_name}
+
+# Artist Calendar
+# https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={your_api_key}
+
+# Similar Artist
+# https://api.songkick.com/api/3.0/artists/{artist_id}/similar_artists.json?apikey={your_api_key}
